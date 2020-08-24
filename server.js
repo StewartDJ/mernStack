059a06882c2d1a3cdbd6e43151c7a1e58e889214
DJ Stewart <deshastewart1992@gmail.com>
2020-08-23 23:24:39 -0700

    origin master

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.port ||  5000;
 
 app.use(cors());
-app.use(express.json()); //converts everything to jason and passes jsonb ack and forth
+app.use(express.json()); 
 
 const uri = process.env.ATLAS_URI
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true})
